5fb7f6f67712b8c77fdc8849b4849b0d0284bfa0
Emmanuel-spring <e.nieto@alumnos.santotomas.cl>
2021-10-18 18:23:47 -0300

    Pizzas Sibaris

---
+++ SibarisPizzasWebParent/SibarisPizzasFrontEnd/src/main/java/com/talentyco/SibarisPizzasFrontEndApplication.java
@@ -0,0 +1,13 @@
+package com.talentyco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SibarisPizzasFrontEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SibarisPizzasFrontEndApplication.class, args);
+	}
+
+}
